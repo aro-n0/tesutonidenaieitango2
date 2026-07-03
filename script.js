d9bbd4e448c4ab9c036cdb584abe7ce7f257c74d
aro-n0 <hira24086@ict.shimanet.ed.jp>
2026-07-03 22:27:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-function showmeaning(){document.getElementById(defenestration).textContent ="人やものを窓から投げ捨てること";
+function showMeaning(){document.getElementById(defenestration).textContent ="人やものを窓から投げ捨てること";
