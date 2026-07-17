e15cb29cb2e13ec30db8a4009ba7cefc23ddc79f
aro-n0 <hira24086@ict.shimanet.ed.jp>
2026-07-17 17:57:22 +0900

    Fix showMeaning function to remove 'hidden' class

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ console.log("ここ読み込まれてる");
 
 function showMeaning() {
     const word = document.getElementById("word");
-
-    word.textContent = "人やものを窓から投げ捨てること";
-    word.classList.toggle("hidden");
+    word.classList.remove("hidden");
 }
